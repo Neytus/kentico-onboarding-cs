cd38ad1598bf1c9bf1100d039fdb15d9471aac44
Neytus <jbielik44@gmail.com>
2017-08-31 09:41:53 +0200

    KA-167 Add a missing file

---
+++ TodoList/TodoList.Contracts/Dependency/IRegisterTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace TodoList.Contracts.Dependency
+{
+    public interface IRegisterTypes
+    {
+        void RegisterType(IUnityContainer container);
+    }
+}
