9987f5b7e80b4a1ac0c03e556bfa0a3576d31fb6
Neytus <jbielik44@gmail.com>
2017-09-08 13:50:44 +0200

    KA-168 Add CurrentTimeService

---
+++ TodoList/TodoList.Contracts/Services/ICurrentTimeService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TodoList.Contracts.Services
+{
+    public interface ICurrentTimeService
+    {
+        DateTime GetCurrentTime();
+    }
+}
---
+++ TodoList/TodoList.Services/Helpers/CurrentTimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TodoList.Contracts.Services;
+
+namespace TodoList.Services.Helpers
+{
+    internal class CurrentTimeService : ICurrentTimeService
+    {
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
