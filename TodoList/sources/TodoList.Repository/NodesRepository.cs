78893cca52fffaba76e1078ad556d9d6722d17a2
Neytus <jbielik44@gmail.com>
2017-09-19 10:06:17 +0200

    KA-167 Inline constants, make getAll method more general

--- TodoList/sources/TodoList.Repository/NodesRepository.cs
+++ TodoList/sources/TodoList.Repository/NodesRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TodoList.Contracts.Api;
 using TodoList.Contracts.Repository;
@@ -7,17 +8,12 @@ namespace TodoList.Repository
 {
     internal class NodesRepository : INodesRepository
     {
-        private static readonly Guid FirstId = new Guid("d237bdda-e6d4-4e46-92db-1a7a0aeb9a72");
-        private static readonly Guid SecondId = new Guid("b84bbcc7-d516-4805-b2e3-20a2df3758a2");
-        private static readonly Guid ThirdId = new Guid("6171ec89-e3b5-458e-ae43-bc0e8ec061e2");
-        private static readonly Guid FourthId = new Guid("b61670fd-33ce-400e-a351-f960230e3aae");
+        private static readonly NodeModel FirstModel = new NodeModel {Id = new Guid("d237bdda-e6d4-4e46-92db-1a7a0aeb9a72"), Text = "poopy"};
+        private static readonly NodeModel SecondModel = new NodeModel {Id = new Guid("b84bbcc7-d516-4805-b2e3-20a2df3758a2"), Text = "GEARS"};
+        private static readonly NodeModel ThirdModel = new NodeModel {Id = new Guid("6171ec89-e3b5-458e-ae43-bc0e8ec061e2"), Text = "Planet Music"};
+        private static readonly NodeModel FourthModel = new NodeModel {Id = new Guid("b61670fd-33ce-400e-a351-f960230e3aae"), Text = "Time to get shwifty"};
 
-        private static readonly NodeModel FirstModel = new NodeModel {Id = FirstId, Text = "poopy"};
-        private static readonly NodeModel SecondModel = new NodeModel {Id = SecondId, Text = "GEARS"};
-        private static readonly NodeModel ThirdModel = new NodeModel {Id = ThirdId, Text = "Planet Music"};
-        private static readonly NodeModel FourthModel = new NodeModel {Id = FourthId, Text = "Time to get shwifty"};
-
-        public async Task<NodeModel[]> GetAllAsync()
+        public async Task<IEnumerable<NodeModel>> GetAllAsync()
             => await Task.FromResult(new[]
             {
                 FirstModel,
